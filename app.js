6261cecaf47d6b0682a412a793e94210c7d38bd8
Muhammadali-fr <boburjonr934@gmail.com>
2025-06-19 17:21:35 +0500

    header-bottom

---
+++ app.js
@@ -0,0 +1,10 @@
+// header-bottom 
+
+let headerLoaction = document.querySelector(".header-loaction");
+let headerLoactionDrop = document.querySelector(".header-loaction-dropdown");
+
+headerLoaction.addEventListener("click" , () => {
+    headerLoactionDrop.classList.toggle("hidden");
+})
+
+// hero-top
