8663344f5dd63878caa282a184ec43b94df08908
alijonovI <normn8113@gmail.com>
2025-06-28 15:41:40 +0500

    hero

--- app.js
+++ app.js
@@ -3,8 +3,7 @@
 let headerLoaction = document.querySelector(".header-loaction");
 let headerLoactionDrop = document.querySelector(".header-loaction-dropdown");
 
-headerLoaction.addEventListener("click" , () => {
+headerLoaction.addEventListener("click", () => {
     headerLoactionDrop.classList.toggle("hidden");
 })
 
-// hero-top
